26c30013ac034e66587217beaad9effa9cc69776
Sadicov Vladimir <sadikoff@gmail.com>
2018-05-04 13:32:03 +0300

    cleanup

--- config/bundles.php
+++ config/bundles.php
@@ -6,6 +6,4 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
